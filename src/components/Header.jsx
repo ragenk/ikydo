4dacdd7f3ded859330d7950b422d4ce568212886
Rafael Aguasvivas Encarnacion <rafael.aguasvivas@tatacommunications.com>
2025-10-05 16:33:01 -0400

    Updating Navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,27 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars } from "@fortawesome/free-solid-svg-icons";
 import { useState } from "react";
+/* import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons"; */
+{/*<FontAwesomeIcon icon={faBars} />*/}
 
 function Header() {
   const [isOpen, setIsOpen] = useState(false);
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  }
   return (
     <header>
-      <button className="ham-btn" aria-label="Menu" onClick={() => setIsOpen(!isOpen)}>
-        <FontAwesomeIcon icon={faBars} />
-      </button>
-      <nav>
-        {isOpen && (<ul>
+      <nav className="navbar">
+        <button className={`hamburger ${isOpen ? "open" : ""}`} aria-label="Menu" onClick={toggleMenu}>
+          <span></span>
+          <span></span>
+          <span></span>
+        </button>
+        <ul className={`nav-links ${isOpen ? "open" : ""}`}>
           <li><a href="#">Home</a></li>
           <li><a href="#">About</a></li>
           <li><a href="#">Projects</a></li>
           <li><a href="#">Contact</a></li>
-        </ul>)}
+        </ul>
       </nav>
     </header>
   );
