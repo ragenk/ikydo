daabecc36fe138d8c1b5836f9c2bdde38c3eec21
Rafael Aguasvivas <100101905+ragenk@users.noreply.github.com>
2025-11-14 23:55:15 -0400

    feat: added react-typed library

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,10 +1,30 @@
+import React from "react";
+import { ReactTyped } from "react-typed";
+import "./Hero.css";
+
 function Hero() {
   return (
-      <main className="hero">
-        <h1>IKYDO</h1>
-        <h2>Front End Developer</h2>
-        <p>Building seamless and modern web experiences</p>
-      </main>
+    <main className="hero">
+      <h1 className="fade-in">IKYDO</h1>
+
+      <h2 className="typed-wrapper fade-in delay-1">
+        <ReactTyped
+          strings={[
+            "Front-End Developer",
+            "React Developer",
+            "WordPress Expert",
+            "Shopify E-commerce Builder",
+          ]}
+          typeSpeed={50}
+          backSpeed={30}
+          loop
+        />
+      </h2>
+
+      <p className="fade-in delay-2">
+        Building seamless and modern web experiences.
+      </p>
+    </main>
   );
 }
 
